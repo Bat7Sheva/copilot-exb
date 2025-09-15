08af3b50dadda891911ac8b31a498b259371148e
TALDOR\batshevae <batshevae@taldor.co.il>
2025-09-15 15:41:32 +0300

    state

--- widgets/free-drawing/src/runtime/widget.tsx
+++ widgets/free-drawing/src/runtime/widget.tsx
@@ -11,12 +11,15 @@ import defaultMessages from './translations/default'
 import DrawIcon from '../../icon.svg'
 import { useEffect } from 'react'
 
+const DRAWING_WIDGET_STATE_KEY = 'drawingWidgetState';
+
 function Widget(props: AllWidgetProps<IMConfig>): React.ReactElement {
   const [currentJimuMapView, setCurrentJimuMapView] = React.useState(null)
   const handleActiveViewChange = (jimuMapView: JimuMapView): void => {
     setCurrentJimuMapView(jimuMapView)
   }
   const drawContainerRef = React.useRef<HTMLDivElement>(null);
+  const [drawingState, setDrawingState] = React.useState<any>(null);
 
   useEffect(() => {
     if (!currentJimuMapView || !drawContainerRef.current) return;
@@ -33,6 +36,40 @@ function Widget(props: AllWidgetProps<IMConfig>): React.ReactElement {
 
     return () => clearInterval(interval);
   }, [currentJimuMapView]);
+
+  // Restore drawing state on mount
+  React.useEffect(() => {
+    const saved = localStorage.getItem(DRAWING_WIDGET_STATE_KEY);
+    if (saved) setDrawingState(JSON.parse(saved));
+  }, []);
+
+  // Save drawing state on change
+  React.useEffect(() => {
+    if (drawingState) {
+      localStorage.setItem(DRAWING_WIDGET_STATE_KEY, JSON.stringify(drawingState));
+    }
+  }, [drawingState]);
+
+    // useEffect(() => {
+    //   if (props.state === WidgetState.Closed) {
+    //     // console.log('הוידג׳ט נסגר');
+  
+    //     setSearchValue('');
+    //     setShowCameraDetails(false);
+    //     setSelectedObj({});
+    //     resetView();
+  
+    //     if (layerData?.targetLayer) {
+    //       setLayerVisibility(layerData.targetLayer, false);
+    //     }
+  
+    //     // Save drawing widget state
+    //     localStorage.setItem(DRAWING_WIDGET_STATE_KEY, JSON.stringify(drawingWidgetState));
+  
+    //     props.dispatch(appActions.closeWidget(props.id));
+  
+    //   }
+    // }, [props.state]);
 
   // visibleElements
   const visibleElements = {} as __esri.SketchVisibleElements
@@ -87,6 +124,9 @@ function Widget(props: AllWidgetProps<IMConfig>): React.ReactElement {
           measurementsInfo={props.config.measurementsInfo.asMutable() as any}
           measurementsUnitsInfos={props.config.measurementsUnitsInfos.asMutable()}
         // other options
+        // העברת סטייט ואירוע שינוי ל-JimuDraw
+        drawingState={drawingState}
+        onDrawingStateChange={handleDrawChange}
         ></JimuDraw>
       </div>
     }
